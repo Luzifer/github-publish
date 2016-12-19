f70612b9e892c4161d403c322c9d3584329ce202
Knut Ahlers <knut@ahlers.me>
2016-12-19 14:09:22 +0100

    Add image signing to docker2aci

--- docker2aci.sh
+++ docker2aci.sh
@@ -32,6 +32,11 @@ if [ -z "${GHUSER}" ]; then
   exit 1
 fi
 
+if [ -z "${GPG_KEY}" ]; then
+  echo "Please set \$GPG_KEY environment variable"
+  exit 1
+fi
+
 DOCKER_IMAGE="${GHUSER,,}/${REPO,,}:${VERSION,,}"
 ACIMAGE="${GHUSER,,}-${REPO,,}-${VERSION,,}.aci"
 
@@ -45,16 +50,15 @@ docker build -t ${DOCKER_IMAGE} ${DOCKERFILE_PATH}
 docker save -o build.docker ${DOCKER_IMAGE}
 
 ./docker2aci build.docker
+gpg2 --local-user ${GPG_KEY} --armor --output ${ACIMAGE}.asc --detach-sig ${ACIMAGE}
 
-# TODO: Sign release
-
-sha256sum ${ACIMAGE} > SHA256SUMS
+sha256sum ${ACIMAGE} ${ACIMAGE}.asc > SHA256SUMS
 
 # Create a drafted release
 github-release release --user ${GHUSER} --repo ${REPO} --tag ${VERSION} --name ${VERSION} --draft || true
 
 # Upload build assets
-for file in ${ACIMAGE} SHA256SUMS; do
+for file in ${ACIMAGE} ${ACIMAGE}.asc SHA256SUMS; do
   github-release upload --user ${GHUSER} --repo ${REPO} --tag ${VERSION} --name ${file} --file ${file}
 done
 
