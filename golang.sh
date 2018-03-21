68ba18f83de3e82e970703eca5de6a7f44556d69
Knut Ahlers <knut@ahlers.me>
2018-03-21 11:32:58 +0100

    Leave build dir at the end for following tassk
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- golang.sh
+++ golang.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
-curl -sL https://raw.githubusercontent.com/Luzifer/github-publish/master/SHA256SUMS | \
-  grep "golang.sh" | sha256sum -c || exit 2
+curl -sL https://raw.githubusercontent.com/Luzifer/github-publish/master/SHA256SUMS |
+	grep "golang.sh" | sha256sum -c || exit 2
 
-( which zip 2>&1 1>/dev/null ) || {
-  ( which apk 2>&1 1>/dev/null ) && apk add --update zip
-  ( which apt-get 2>&1 1>/dev/null ) && apt-get install -y zip
+(which zip 2>&1 1>/dev/null) || {
+	(which apk 2>&1 1>/dev/null) && apk add --update zip
+	(which apt-get 2>&1 1>/dev/null) && apt-get install -y zip
 }
 
-function step {
-  echo "===> $@..."
+function step() {
+	echo "===> $@..."
 }
 
 VERSION=$(git describe --tags --exact-match || echo "ghpublish__notags")
 PWD=$(pwd)
-godir=${PWD/${GOPATH}\/src\/}
+godir=${PWD/${GOPATH}\/src\//}
 REPO=${REPO:-$(echo ${godir} | cut -d '/' -f 3)}
 GHUSER=${GHUSER:-$(echo ${godir} | cut -d '/' -f 2)}
 ARCHS=${ARCHS:-"linux/amd64 linux/arm darwin/amd64 windows/amd64"}
@@ -31,46 +31,49 @@ step Test code
 go vet ${PACKAGES}
 go test ${PACKAGES}
 
+step Cleanup build directory if present
+rm -rf ${BUILD_DIR}
+
 step Compile program
 mkdir ${BUILD_DIR}
 gox -ldflags="-X main.version=${VERSION}" -osarch="${ARCHS}" \
-  -output="${BUILD_DIR}/{{.Dir}}_{{.OS}}_{{.Arch}}"\
-  ${PACKAGES}
+	-output="${BUILD_DIR}/{{.Dir}}_{{.OS}}_{{.Arch}}" \
+	${PACKAGES}
 
 step Generate binary SHASUMs
 cd ${BUILD_DIR}
-sha256sum * > SHA256SUMS
+sha256sum * >SHA256SUMS
 
 step Packing archives
 for file in *; do
-  if [ "${file}" = "SHA256SUMS" ]; then
-    continue
-  fi
+	if [ "${file}" = "SHA256SUMS" ]; then
+		continue
+	fi
 
-  if [[ ${file} == *linux* ]]; then
-    tar -czf "${file%%.*}.tar.gz" "${file}"
-  else
-    zip "${file%%.*}.zip" "${file}"
-  fi
+	if [[ ${file} == *linux* ]]; then
+		tar -czf "${file%%.*}.tar.gz" "${file}"
+	else
+		zip "${file%%.*}.zip" "${file}"
+	fi
 
-  rm "${file}"
+	rm "${file}"
 done
 
 step Generate archive SHASUMs
-sha256sum * >> SHA256SUMS
-grep -v 'SHA256SUMS' SHA256SUMS > SHA256SUMS.tmp
+sha256sum * >>SHA256SUMS
+grep -v 'SHA256SUMS' SHA256SUMS >SHA256SUMS.tmp
 mv SHA256SUMS.tmp SHA256SUMS
 
 step Publish builds to Github
 
-if ( test "${VERSION}" == "ghpublish__notags" ); then
-  echo "No tag present, stopping build now."
-  exit 0
+if (test "${VERSION}" == "ghpublish__notags"); then
+	echo "No tag present, stopping build now."
+	exit 0
 fi
 
 if [ -z "${GITHUB_TOKEN}" ]; then
-  echo "Please set \$GITHUB_TOKEN environment variable"
-  exit 1
+	echo "Please set \$GITHUB_TOKEN environment variable"
+	exit 1
 fi
 
 step Create a drafted release
@@ -78,14 +81,11 @@ github-release release --user ${GHUSER} --repo ${REPO} --tag ${DEPLOYMENT_TAG} -
 
 step Upload build assets
 for file in *; do
-  echo "- ${file}"
-  github-release upload --user ${GHUSER} --repo ${REPO} --tag ${DEPLOYMENT_TAG} --name ${file} --file ${file}
+	echo "- ${file}"
+	github-release upload --user ${GHUSER} --repo ${REPO} --tag ${DEPLOYMENT_TAG} --name ${file} --file ${file}
 done
 
 echo -e "\n\n=== Recorded checksums ==="
 cat SHA256SUMS
 
-step Cleanup build directory
 cd -
-rm -rf ${BUILD_DIR}
-
