7b9800a5dfaf8078445f62ef8df0ffa75ffe59f5
Knut Ahlers <knut@ahlers.me>
2019-07-10 12:46:51 +0200

    Pull dependencies outside working dir
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- golang.sh
+++ golang.sh
@@ -13,24 +13,26 @@ function step() {
 	echo "===> $@..."
 }
 
-VERSION=$(git describe --tags --always || echo "dev")
-PWD=$(pwd)
-godir=${PWD/${GOPATH}\/src\//}
-REPO=${REPO:-$(echo ${godir} | cut -d '/' -f 3)}
-GHUSER=${GHUSER:-$(echo ${godir} | cut -d '/' -f 2)}
 ARCHS=${ARCHS:-"linux/amd64 linux/arm darwin/amd64 windows/amd64"}
-DEPLOYMENT_TAG=${DEPLOYMENT_TAG:-${VERSION}}
-PACKAGES=${PACKAGES:-$(echo ${godir} | cut -d '/' -f 1-3)}
 BUILD_DIR=${BUILD_DIR:-.build}
+DEPLOYMENT_TAG=${DEPLOYMENT_TAG:-${VERSION}}
 DRAFT=${DRAFT:-true}
 FORCE_SKIP_UPLOAD=${FORCE_SKIP_UPLOAD:-false}
+GHUSER=${GHUSER:-$(echo ${godir} | cut -d '/' -f 2)}
+godir=${PWD/${GOPATH}\/src\//}
 MOD_MODE=${MOD_MODE:-}
+PACKAGES=${PACKAGES:-$(echo ${godir} | cut -d '/' -f 1-3)}
+PWD=$(pwd)
+REPO=${REPO:-$(echo ${godir} | cut -d '/' -f 3)}
+VERSION=$(git describe --tags --always || echo "dev")
 
 go version
 
 step "Retrieve dependencies"
+pushd ${GOPATH}
 go get github.com/aktau/github-release
 go get github.com/mitchellh/gox
+popd
 
 step "Test code"
 go vet ${PACKAGES}
